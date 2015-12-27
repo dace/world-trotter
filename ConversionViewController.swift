e868c9e92f5eef48063405b7b91fef5745ef8adf
Dace Willmott <dacewillmott@gmail.com>
2015-12-27 00:31:09 -0500

    add conversion controller and connect with storyboard

---
+++ ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Dace Willmott on 12/27/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+  
+}
