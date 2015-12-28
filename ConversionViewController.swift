fcc4c8bcca75edca47a44180ed48382e82873ccf
hidace <dacewillmott@gmail.com>
2015-12-28 09:06:23 -0500

    add formula that converts from f to c degrees

--- ConversionViewController.swift
+++ ConversionViewController.swift
@@ -9,5 +9,44 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
-  
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var farenheightValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    
+    var celsiusValue: Double? {
+        if let value = farenheightValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheightFieldEditingChanged(textField: UITextField) {
+      if let text = textField.text where !text.isEmpty {
+        celsiusLabel.text = text
+      } else {
+        celsiusLabel.text = "???"
+      }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
 }
+
