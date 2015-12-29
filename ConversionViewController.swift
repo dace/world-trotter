a635d440033b5c1df1ec71deaae8906d01d7b755
Dace Willmott <willmott@brooklynunited.com>
2015-12-29 12:43:52 -0500

    add number formatter for 1 decimal conversion

--- ConversionViewController.swift
+++ ConversionViewController.swift
@@ -9,44 +9,54 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
-    
-    @IBOutlet var celsiusLabel: UILabel!
-    @IBOutlet var textField: UITextField!
-    
-    var farenheightValue: Double? {
-        didSet {
-            updateCelsiusLabel()
-        }
+  
+  @IBOutlet var celsiusLabel: UILabel!
+  @IBOutlet var textField: UITextField!
+  
+  var farenheightValue: Double? {
+    didSet {
+      updateCelsiusLabel()
     }
-    
-    
-    var celsiusValue: Double? {
-        if let value = farenheightValue {
-            return (value - 32) * (5/9)
-        } else {
-            return nil
-        }
+  }
+  
+  
+  var celsiusValue: Double? {
+    if let value = farenheightValue {
+      return (value - 32) * (5/9)
+    } else {
+      return nil
     }
-    
-    func updateCelsiusLabel() {
-        if let value = celsiusValue {
-            celsiusLabel.text = "\(value)"
-        } else {
-            celsiusLabel.text = "???"
-        }
+  }
+  
+  func updateCelsiusLabel() {
+    if let value = celsiusValue {
+      celsiusLabel.text = numberFormatter.stringFromNumber(value)
+    } else {
+      celsiusLabel.text = "???"
     }
-    
-    @IBAction func fahrenheightFieldEditingChanged(textField: UITextField) {
-      if let text = textField.text where !text.isEmpty {
-        celsiusLabel.text = text
-      } else {
-        celsiusLabel.text = "???"
-      }
+  }
+  
+  @IBAction func fahrenheightFieldEditingChanged(textField: UITextField) {
+    if let text = textField.text, value = Double(text) {
+      farenheightValue = value
+    } else {
+      farenheightValue = nil
     }
-    
-    @IBAction func dismissKeyboard(sender: AnyObject) {
-        textField.resignFirstResponder()
-    }
-    
+  }
+  
+  @IBAction func dismissKeyboard(sender: AnyObject) {
+    textField.resignFirstResponder()
+  }
+  
+  let numberFormatter: NSNumberFormatter = {
+    let nf = NSNumberFormatter()
+    nf.numberStyle = .DecimalStyle
+    nf.minimumFractionDigits = 0
+    nf.maximumFractionDigits = 1
+    return nf
+  }()
+  
+  
+  
 }
 
